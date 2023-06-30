ac17f23c5ba5825f27b361af43d1148b6a60555c
Alexander Neal Yagüe <alexander.nealyague@usp.ceu.es>
2023-06-30 02:05:08 +0200

    eur € currency changed, and new member Added

--- meetingApp copia/popup.js
+++ meetingApp copia/popup.js
@@ -1,43 +1,9 @@
 // Code for the popup window
-// In popup.js
 
 const membersList = document.getElementById("members-list");
-const totalCostElement = document.getElementById("total-cost");
+const totalCost = document.getElementById("total-cost");
 
 let members = [];
-let meetingDuration = 2; // Meeting duration in hours
-
-function calculateMeetingCost() {
-  let totalCost = 0;
-  members.forEach((member) => {
-    totalCost += member.costPerHour * meetingDuration;
-  });
-  return totalCost.toFixed(2);
-}
-
-function renderMembersList() {
-  membersList.innerHTML = "";
-
-  members.forEach((member) => {
-    const memberEl = document.createElement("div");
-    memberEl.classList.add("member");
-    memberEl.innerHTML = `
-      <div class="member-icon"></div>
-      <div class="member-name">${member.name}</div>
-      <div class="member-cost">$${(member.costPerHour * meetingDuration).toFixed(2)}</div>
-    `;
-    membersList.appendChild(memberEl);
-  });
-
-  if (members.length === 0) {
-    const emptyMessageEl = document.createElement("li");
-    emptyMessageEl.classList.add("member-empty");
-    emptyMessageEl.textContent = "No members added yet";
-    membersList.appendChild(emptyMessageEl);
-  }
-
-  totalCostElement.textContent = `Total Cost: $${calculateMeetingCost()}`;
-}
 
 // Load and parse CSV file
 const csvFilePath = 'team_salaries.csv';
@@ -48,8 +14,8 @@ fetch(csvFilePath)
     const parsedData = Papa.parse(data, { header: true }).data;
     parsedData.forEach((row) => {
       const name = row.Name;
-      const salary = parseInt(row.Salary);
-      const costPerHour = salary / 52 / 40;
+      const salary = parseFloat(row.Salary);
+      const costPerHour = (salary / 52 / 40).toFixed(2);
 
       const member = {
         name: name,
@@ -61,7 +27,40 @@ fetch(csvFilePath)
     });
 
     renderMembersList();
+    calculateTotalCost();
   })
   .catch((error) => {
     console.error('Error:', error);
   });
+
+function renderMembersList() {
+  membersList.innerHTML = "";
+
+  members.forEach((member) => {
+    const memberEl = document.createElement("div");
+    memberEl.classList.add("member");
+    memberEl.innerHTML = `
+      <div class="member-icon"></div>
+      <div class="member-name">${member.name}</div>
+      <div class="member-cost">${member.costPerHour} €/hr</div>
+    `;
+    membersList.appendChild(memberEl);
+  });
+
+  if (members.length === 0) {
+    const emptyMessageEl = document.createElement("li");
+    emptyMessageEl.classList.add("member-empty");
+    emptyMessageEl.textContent = "No members added yet";
+    membersList.appendChild(emptyMessageEl);
+  }
+}
+
+function calculateTotalCost() {
+  let total = 0;
+
+  members.forEach((member) => {
+    total += parseFloat(member.costPerHour) * 2; // Assuming meeting duration of 2 hours
+  });
+
+  totalCost.textContent = `Total Cost: ${total.toFixed(2)} €`;
+}
